d1e4e5ec8c629a0c2ce5d3d3ba03324f99a77082
cecil john tantay <cjbtantay@gmail.com>
2019-07-01 11:57:43 -0700

    route

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import {Route} from 'react-router-dom'
 
 class Dashboard extends React.Component {
   render() {
-    return <h1>Dashboard</h1>;
+    return (
+      <>
+        <SideNavigation />
+        <Route exact path="/dashboard/" component={DashboardHome}/>
+        <Route path="/dashboard/logbook/" component={LogBook} />
+        {/* <Route path="/dashboard/ac-form" component={} />
+        <Route path="/dashboard/flight-form" component={} /> */}
+        <Route component={} />
+      </>
+    );
   }
 }
 
