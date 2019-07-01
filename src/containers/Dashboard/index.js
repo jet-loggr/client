55536f5dd7482fc39022469dfe7ca81ccd6751d1
cecil john tantay <cjbtantay@gmail.com>
2019-07-01 12:08:52 -0700

    dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,19 +1,36 @@
 import React from "react";
-import {Route} from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
+import ErrorPage from "../ErrorPage";
+import Logbook from "../LogBook";
+import DashboardHome from "../DashboardHome";
+import SideNavigation from "../SideNavigation";
 
 class Dashboard extends React.Component {
   render() {
     return (
       <>
         <SideNavigation />
-        <Route exact path="/dashboard/" component={DashboardHome}/>
-        <Route path="/dashboard/logbook/" component={LogBook} />
-        {/* <Route path="/dashboard/ac-form" component={} />
-        <Route path="/dashboard/flight-form" component={} /> */}
-        <Route component={} />
+        <Switch>
+          <Route exact path="/dashboard/" component={DashboardHome} />
+          <Route path="/dashboard/logbook/" component={Logbook} />
+          {/* <Route path="/dashboard/ac-form" component={} />
+          <Route path="/dashboard/flight-form" component={} /> */}
+          <Route
+            path="/dashboard/*"
+            render={props => <ErrorPage {...props} />}
+          />
+        </Switch>
       </>
     );
   }
 }
 
 export default Dashboard;
+
+/**
+ *
+ *
+ * / Landing
+ * /About
+ * /Dashboard
+ */
