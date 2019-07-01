b74266e58bb9c5986731938864723ae29b6de5f5
cecil john tantay <cjbtantay@gmail.com>
2019-07-01 13:38:20 -0700

    changes

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -4,6 +4,7 @@ import ErrorPage from "../ErrorPage";
 import { LogBook, EnhancedTable } from "../LogBook/LogBook";
 import DashboardHome from "../../components/DashboardHome";
 import SideNavigation from "../../components/SideNavigation";
+import FlightForm from "../../components/FlightForm";
 
 class Dashboard extends React.Component {
   render() {
@@ -20,8 +21,8 @@ class Dashboard extends React.Component {
             path="/dashboard/enhanced-logbook"
             render={props => <EnhancedTable {...props} />}
           />
-          {/* <Route path="/dashboard/ac-form" component={} />
-          <Route path="/dashboard/flight-form" component={} /> */}
+          {/* <Route path="/dashboard/ac-form" component={} /> */}
+          <Route path="/dashboard/flight-form" component={FlightForm} />
           <Route
             path="/dashboard/*"
             render={props => <ErrorPage {...props} />}
