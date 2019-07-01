16c1f3ba0116e46a29241384880261b248ec8ef8
joepound <14357797+joepound@users.noreply.github.com>
2019-07-01 12:46:31 -0600

    Add main 404 (Not Found) error page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import Dashboard from "./containers/Dashboard";
 import Callback from "./containers/auth-zero/Callback/Callback";
 import LandingPage from "./containers/LandingPage";
+import ErrorPage from "./containers/ErrorPage";
 import Auth from "./containers/auth-zero/Auth/Auth.js";
+import "./styles/App.scss";
 
 const auth = new Auth();
 
@@ -32,6 +34,10 @@ function App() {
         <Route
           path="/dashboard"
           render={props => <Dashboard {...props} auth={auth} />}
+        />
+        <Route
+          path="*"
+          render={props => <ErrorPage {...props} auth={auth} />}
         />
       </Switch>
     </div>
---
+++ src/containers/ErrorPage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ErrorPage = props => {
+  return (
+    <h1 className="error-page__title">Page Not Found.</h1>
+  );
+};
+
+export default ErrorPage;
