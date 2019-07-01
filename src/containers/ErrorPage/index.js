ad38e9ac4b2b170be9d7732ab2addb5e20a7898b
joepound <14357797+joepound@users.noreply.github.com>
2019-07-01 13:01:38 -0600

    Add 'Go Back' button to main 404 error page

--- src/containers/ErrorPage/index.js
+++ src/containers/ErrorPage/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import history from "../auth-zero/history";
 
 const ErrorPage = props => {
   return (
-    <h1 className="error-page__title">Page Not Found.</h1>
+    <>
+      <h1 className="error-page__title">Page Not Found.</h1>
+      <button className="error-page__go-back-btn" onClick={history.goBack}>Go Back</button>
+    </>
   );
 };
 
