9fe9a8064764e5a9e48694c67621b1cd769dc04e
Zooheck <davidscottflack@gmail.com>
2019-07-01 14:33:31 -0500

    Add material-datatable

--- src/containers/LogBook/LogBookTemplate.js
+++ src/containers/LogBook/LogBookTemplate.js
@@ -22,24 +22,24 @@ import Switch from "@material-ui/core/Switch";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FilterListIcon from "@material-ui/icons/FilterList";
 
-function createData(id, date, flightNo, tripNo, duration, aircraft) {
-  return { id, date, flightNo, tripNo, duration, aircraft };
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData(1, 305, 3.7, 67, 4.3, "Boeing"),
-  createData(2, 452, 25.0, 51, 4.9, "Boeing"),
-  createData(3, 262, 16.0, 24, 6.0, "Boeing"),
-  createData(4, 159, 6.0, 24, 4.0, "Boeing"),
-  createData(5, 356, 16.0, 49, 3.9, "Boeing"),
-  createData(6, 408, 3.2, 87, 6.5, "Boeing"),
-  createData(7, 237, 9.0, 37, 4.3, "Boeing"),
-  createData(8, 375, 0.0, 94, 0.0, "Boeing"),
-  createData(9, 518, 26.0, 65, 7.0, "Boeing"),
-  createData(10, 392, 0.2, 98, 0.0, "Boeing"),
-  createData(11, 318, 0, 81, 2.0, "Boeing"),
-  createData(12, 360, 19.0, 9, 37.0, "Boeing"),
-  createData(13, 437, 18.0, 63, 4.0, "Boeing")
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Donut", 452, 25.0, 51, 4.9),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Honeycomb", 408, 3.2, 87, 6.5),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Jelly Bean", 375, 0.0, 94, 0.0),
+  createData("KitKat", 518, 26.0, 65, 7.0),
+  createData("Lollipop", 392, 0.2, 98, 0.0),
+  createData("Marshmallow", 318, 0, 81, 2.0),
+  createData("Nougat", 360, 19.0, 9, 37.0),
+  createData("Oreo", 437, 18.0, 63, 4.0)
 ];
 
 function desc(a, b, orderBy) {
@@ -70,16 +70,15 @@ function getSorting(order, orderBy) {
 
 const headRows = [
   {
-    id: "id",
-    numeric: true,
+    id: "name",
+    numeric: false,
     disablePadding: true,
-    label: "Flight ID"
+    label: "Dessert (100g serving)"
   },
-  { id: "date", numeric: true, disablePadding: false, label: "Date" },
-  { id: "flightNo", numeric: true, disablePadding: false, label: "Flight No." },
-  { id: "tripNo", numeric: true, disablePadding: false, label: "Trip No." },
-  { id: "duration", numeric: true, disablePadding: false, label: "Duration" },
-  { id: "aircraft", numeric: false, disablePadding: false, label: "Aircraft" }
+  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
+  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
+  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
+  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" }
 ];
 
 function EnhancedTableHead(props) {
@@ -227,7 +226,7 @@ const useStyles = makeStyles(theme => ({
 export const EnhancedTable = () => {
   const classes = useStyles();
   const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("date");
+  const [orderBy, setOrderBy] = React.useState("calories");
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
@@ -241,19 +240,19 @@ export const EnhancedTable = () => {
 
   function handleSelectAllClick(event) {
     if (event.target.checked) {
-      const newSelecteds = rows.map(n => n.id);
+      const newSelecteds = rows.map(n => n.name);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   }
 
-  function handleClick(event, id) {
-    const selectedIndex = selected.indexOf(id);
+  function handleClick(event, name) {
+    const selectedIndex = selected.indexOf(name);
     let newSelected = [];
 
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
+      newSelected = newSelected.concat(selected, name);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -280,7 +279,7 @@ export const EnhancedTable = () => {
     setDense(event.target.checked);
   }
 
-  const isSelected = id => selected.indexOf(id) !== -1;
+  const isSelected = name => selected.indexOf(name) !== -1;
 
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
@@ -307,17 +306,17 @@ export const EnhancedTable = () => {
               {stableSort(rows, getSorting(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.id);
+                  const isItemSelected = isSelected(row.name);
                   const labelId = `enhanced-table-checkbox-${index}`;
 
                   return (
                     <TableRow
                       hover
-                      onClick={event => handleClick(event, row.id)}
+                      onClick={event => handleClick(event, row.name)}
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.id}
+                      key={row.name}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
@@ -332,13 +331,12 @@ export const EnhancedTable = () => {
                         scope="row"
                         padding="none"
                       >
-                        {row.id}
+                        {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.date}</TableCell>
-                      <TableCell align="right">{row.flightNo}</TableCell>
-                      <TableCell align="right">{row.tripNo}</TableCell>
-                      <TableCell align="right">{row.duration}</TableCell>
-                      <TableCell align="right">{row.aircraft}</TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
                     </TableRow>
                   );
                 })}
