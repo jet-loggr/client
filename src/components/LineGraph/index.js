1a5bfd8903ec90b3893470ceac74125ab59108cf
joepound <14357797+joepound@users.noreply.github.com>
2019-07-02 08:12:29 -0600

    Add line graph component (displays daily flight count of user for current week)

---
+++ src/components/LineGraph/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import { LineChart } from "react-chartkick";
+import "chart.js";
+
+const DailyFlightCountInCurrentWeekChart = props => {
+  const [lineChartProps, setLineChartProps] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get("/api/flights/line-graph")
+      .then(res => {
+        console.log(res.data);
+
+        const lineChartData = {};
+        res.data.forEach(day => {
+          lineChartData[day.date] = day.count;
+        });
+
+        setLineChartProps(lineChartData);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <LineChart
+      {...props.options}
+      data={lineChartProps}
+      messages={{ empty: "No flight data available so far for this week." }}
+    />
+  );
+};
+
+export default DailyFlightCountInCurrentWeekChart;
