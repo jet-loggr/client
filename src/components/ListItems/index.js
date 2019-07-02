80ade9a039148526b00785b4574dfa639638ab88
Zooheck <davidscottflack@gmail.com>
2019-07-02 12:03:47 -0500

    Remove unused imports.

--- src/components/ListItems/index.js
+++ src/components/ListItems/index.js
@@ -3,11 +3,6 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import SettingsIcon from "@material-ui/icons/Settings";
-import PeopleIcon from "@material-ui/icons/People";
-import BarChartIcon from "@material-ui/icons/BarChart";
-import AssignmentIcon from "@material-ui/icons/Assignment";
 import { NavLink } from "react-router-dom";
 
 export const mainListItems = (
