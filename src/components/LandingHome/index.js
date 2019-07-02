e29826865760bdc6546dc827479b54d7255fd260
Javontay McElroy <dinomightnetwork@gmail.com>
2019-07-02 12:13:56 -0500

    Styled the landing page.

--- src/components/LandingHome/index.js
+++ src/components/LandingHome/index.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./landingpage.scss";
+
+import heroImg from "./hero_img.png";
+import arrow from "./arrow.svg";
 
 const index = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="landing-page-container">
+      <img src={heroImg} alt="plane" className="hero-image" />
+      <div className="hero-content">
+        <h1 className="title">JetLogr</h1>
+        <h2 className="caption">
+          The best app for pilots to log and track their hours.
+        </h2>
+        <p className="sub-caption">
+          We know how crucial and important it is for you to be able to quickly
+          log and keep track of your flight hours, aircraft, passengers, and
+          more. So we’ve made it easy!
+        </p>
+      </div>
+      <div className="CTA">
+        <div className="content">
+          <h2>Sign up today and start logging!</h2>
+          <img src={arrow} className="arrow" alt="arrow" />
+        </div>
+        <Link to="/login" className="get-started">
+          <div>
+            <h1>Get started</h1>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/LandingNav/index.js
+++ src/components/LandingNav/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import logo from "./logo.svg";
+
 const index = ({ login }) => {
   const submitHandler = () => {
     login();
   };
   return (
-    <div className="nav-container">
-      <div className="logo-container">
-        <img src={require("./plane-departure-solid.svg")} alt="" />
-        <h1>Jet Logr</h1>
-      </div>
-      <div className="menu">
-        <h3>Features</h3>
-        <h3>About</h3>
-        <h3>Careers</h3>
-        <button onClick={submitHandler}>Login</button>
+    <div className="navigation-container">
+      <div className="nav-flex">
+        <Link to="/">
+          <img src={logo} className="logo" alt="logo" />
+        </Link>
+        <div className="nav-links">
+          <Link to="/" className="links">
+            Home
+          </Link>
+          <Link to="/Features" className="links">
+            Features
+          </Link>
+          <Link to="/About" className="links">
+            About
+          </Link>
+          <Link to="/Careers" className="links">
+            Careers
+          </Link>
+          <button className="login-btn">Log In</button>
+        </div>
       </div>
     </div>
   );
